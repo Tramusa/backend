de694274d5ddc6f709feb2978b5d13dad011b61d
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2023-08-15 09:11:37 -0600

    Update Others.php

--- app/Models/Others.php
+++ app/Models/Others.php
@@ -10,6 +10,6 @@ class Others extends Model
     use HasFactory;
                                                                                            
     protected $fillable = [ 
-        'user_id', 'state_civil', 'regimen', 'sex', 'birthdate', 'place_birth', 'nationality', 'scholarship', 'title', 'ine', 'rfc', 'curp', 'socia_health', 'umf', 'weight', 'height', 'blood_type'   
+        'user_id', 'state_civil', 'regimen', 'sex', 'birthdate', 'place_birth', 'nationality', 'scholarship', 'title', 'ine', 'license', 'type_license', 'expiration_license', 'expiration_psychophysical', 'expiration_general', 'rfc', 'curp', 'socia_health', 'umf', 'weight', 'height', 'blood_type'   
     ];
 }
