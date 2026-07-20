aec4f912174a7d8b7e4b8743784cc4cb664bae9a
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2026-07-20 15:33:35 -0600

    Update InventoryDetailsController.php

--- app/Http/Controllers/InventoryDetailsController.php
+++ app/Http/Controllers/InventoryDetailsController.php
@@ -9,11 +9,21 @@ class InventoryDetailsController extends Controller
 {
     public function index(Request $request)
     {
-        $inventoryId = $request->input('id_inventory');
-    
-        // Filtrar por id_inventory si se proporciona en la solicitud
-        $details = InventoryDetails::where('id_inventory', $inventoryId)->with('product')->get();
-    
-        return response()->json($details);
+        try {
+            $inventoryId = $request->input('id_inventory');
+
+            $details = InventoryDetails::where('id_inventory', $inventoryId)
+                        ->with('product')
+                        ->get();
+
+            return response()->json($details);
+        } catch (\Throwable $e) {
+
+            return response()->json([
+                'message' => $e->getMessage(),
+                'line' => $e->getLine(),
+                'file' => $e->getFile(),
+            ],500);
+        }
     }
 }
